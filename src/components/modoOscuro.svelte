db73276baddfefa7becc0fa4039b9529284583f8
Lauti <lautarosaintmartin2@gmail.com>
2026-04-14 17:39:20 -0300

    feat: add new components for skills, interests, and projects; improve layout and styling across pages

--- src/components/modoOscuro.svelte
+++ src/components/modoOscuro.svelte
@@ -1,22 +1,27 @@
 <script>
-    let oscuro = $state(false)
+    let oscuro = $state(false);
 
-    if(typeof window !== 'undefined' && localStorage.getItem('oscuro') === 'true'){
-        oscuro = true
-        document.documentElement.classList.add('dark-mode')
+    if (
+        typeof window !== "undefined" &&
+        localStorage.getItem("oscuro") === "true"
+    ) {
+        oscuro = true;
+        document.documentElement.classList.add("dark-mode");
     }
 
-    function modoOscuro(){
-        oscuro = !oscuro
-       
-        document.documentElement.classList.toggle('dark-mode')
-        localStorage.setItem('oscuro', oscuro)
+    function modoOscuro() {
+        oscuro = !oscuro;
+
+        document.documentElement.classList.toggle("dark-mode");
+        localStorage.setItem("oscuro", oscuro);
     }
 </script>
 
-<button 
+<button
     onclick={modoOscuro}
-    class= {oscuro ? "fixed top-6 right-6 bg-white border border-slate-200 px-4 py-2 rounded-full shadow-lg text-black" : "fixed top-6 right-6 bg-black border border-slate-200 px-4 py-2 rounded-full shadow-lg text-white"}>
-    
-    {oscuro ? '☀︎' : '☾'}
+    class={oscuro
+        ? "fixed top-6 right-6 bg-white border border-slate-200 px-4 py-2 rounded-full shadow-lg text-black"
+        : "fixed top-6 right-6 bg-black border border-slate-200 px-4 py-2 rounded-full shadow-lg text-white"}
+>
+    {oscuro ? "☀︎" : "☾"}
 </button>
